9113a7fdb215400c689875ecddbee0eec3e7a5d2
Joonas Yliaho <joonas.yliaho@gmail.com>
2017-07-29 21:05:39 +0300

    initial commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   * router config 
+   */
+  router: {
+    base: '/legoff-landing/'
+  },
+  /*
+   * Headers of the page
+   */
   head: {
     title: 'starter',
     meta: [
@@ -14,16 +20,16 @@ module.exports = {
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
+   * Customize the progress-bar color
+   */
   loading: { color: '#3B8070' },
   /*
-  ** Build configuration
-  */
+   * Build configuration
+   */
   build: {
     /*
-    ** Run ESLINT on save
-    */
+     * Run ESLINT on save
+     */
     extend (config, ctx) {
       if (ctx.isClient) {
         config.module.rules.push({
