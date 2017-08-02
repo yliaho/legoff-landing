5b330c0f4384799fcfd4890424dbabcef3e4ef02
Joonas Yliaho <joonas.yliaho@gmail.com>
2017-08-02 15:05:13 +0300

    carousell animation horizontally instead of vertically

--- components/ImageCarousell.vue
+++ components/ImageCarousell.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="carousell">
-    <transition name="fade">
+    <transition :name="`${side}-fade`">
       <div class="image"
           v-for="(image, index) in images(side)" 
           :key="index"
@@ -59,22 +59,42 @@ export default {
   }
 
   /*
-   * Transition animation
+   * Transition animation LEFT
    */
-  .fade-leave-active {
+  .left-fade-leave-active {
     transition: all 1s cubic-bezier(0.165, 0.84, 0.44, 1);
     transform-origin: 100% 50%;
   }
-  .fade-enter-active, {
+  .left-fade-enter-active, {
     transition: all 1s cubic-bezier(0.165, 0.84, 0.44, 1);
     transform-origin: 100% 50%;   
   }
-  .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-    transform: translateY(-100vh) scale(1);
+  .left-fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    transform: translateX(-50vw) scale(1);
     opacity: .4;
   }
-  .fade-enter {
-    transform: translateY(100vh);
-    opacity: 0;
+  .left-fade-enter {
+    transform: translateX(50vw);
+    opacity: 1;
+  }
+
+  /*
+   * Transition animation RIGHT
+   */
+  .right-fade-leave-active {
+    transition: all 1s cubic-bezier(0.165, 0.84, 0.44, 1);
+    transform-origin: 100% 50%;
+  }
+  .right-fade-enter-active, {
+    transition: all 1s cubic-bezier(0.165, 0.84, 0.44, 1);
+    transform-origin: 100% 50%;   
+  }
+  .right-fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    transform: translateX(50vw) scale(1);
+    opacity: .4;
+  }
+  .right-fade-enter {
+    transform: translateX(-50vw);
+    opacity: 1;
   }
 </style>
